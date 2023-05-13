bb5e9a531edf99cd75f5815d1f368bd2faa3e717
Samuel Mathes <samuel.mathes@gmx.de>
2023-05-13 17:00:11 +0200

    Add test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MainTest {
+
+    @Test
+    void test() {
+        Main.main(new String[]{});
+    }
+
+}
